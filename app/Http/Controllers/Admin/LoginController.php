a2c7f26d877f9f837d82b3299fe356424d52167f
MohammedHassona99 <mohammedhassona99@hotmail.com>
2023-08-26 11:46:55 +0300

    login-tinker

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use App\Models\Admin;
 
 class LoginController extends Controller
 {
@@ -12,6 +13,7 @@ class LoginController extends Controller
     {
         return view('admin.auth.login');
     }
+
     public function login(Request $request)
     {
         $rule = [
